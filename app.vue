831bb8efd7b005b0b1bb0084ae08ab5185bd0bd2
Bedir Demir <bedir@proton.me>
2024-03-07 22:24:10 +0300

    refactor: color mode transition

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <NuxtLoadingIndicator :height="3" :throttle="10" />
-  <div class="bg-white dark:bg-gray-900 dark:text-white">
+  <div
+    class="bg-white transition-colors duration-75 dark:bg-gray-900 dark:text-white"
+  >
     <div class="max-w-screen-md mx-auto pt-6">
       <AppHeader />
       <main class="min-h-screen py-10 lg:pt-16 mx-6">
